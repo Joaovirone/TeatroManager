78748a150fce230677a379009256d62e1b9e2205
joao <jovmamikl@gmail.com>
2026-02-12 07:59:58 -0300

    feat: Implementando commits iniciais do projeto

---
+++ src/test/java/Manager/TeatroManager/TeatroManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Manager.TeatroManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatroManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
