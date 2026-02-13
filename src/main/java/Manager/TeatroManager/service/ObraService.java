95c7fac355a7f583252134d2388ee19cba9fbb40
joao <jovmamikl@gmail.com>
2026-02-13 15:02:13 -0300

    refactor: ajuste no método de salvar a obra e implementação de buscar obra por nome feita com tratamento de exceção

--- src/main/java/Manager/TeatroManager/service/ObraService.java
+++ src/main/java/Manager/TeatroManager/service/ObraService.java
@@ -1,12 +1,14 @@
 package Manager.TeatroManager.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import Manager.TeatroManager.entity.Obra;
+import Manager.TeatroManager.exception.EntityNotFoundException;
 import Manager.TeatroManager.repository.ObraRepository;
 
 @Service
@@ -15,28 +17,26 @@ public class ObraService {
     @Autowired
     private ObraRepository repository;
 
-    public List<Obra> listarTodasAsObras(){
-
-        // Aqui você poderia ordenar por data, nota, etc.
-        // Ex: return repository.findAll(Sort.by(Sort.Direction.DESC, "dataAssistida"));
-
-        return repository.findAll();
-    }
-
-    public Obra buscarObraPorNome(String nome){
-
-        return repository.findByNomeContainigIgnoreCase(nome)
-                            .orElseThrow(() -> new RuntimeException("Obra não encontrada com nome: " + nome));
-
-    }
-    
     @Transactional
-    public Obra salvar(Obra obra){
-
+    public Obra salvarObra(Obra obra){
         
-
-        obra.setId(null);
-
         return repository.save(obra);
     }
+
+    @Transactional(readOnly = true)
+    public List<Obra> buscarObraPorNome (String nome){
+        
+      List<Obra> obras = repository.findByNomeContainigIgnoreCase(nome);
+
+      if(obras.isEmpty()){
+        throw new EntityNotFoundException(
+            String.format("A obra de nome | %s | -- Não está cadastrada.", nome)
+        );
+      }
+
+      return obras;
+        
+    }
+
+
 }
