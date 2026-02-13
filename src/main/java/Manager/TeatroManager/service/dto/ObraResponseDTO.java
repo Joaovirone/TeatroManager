686e9c6cdc805ce6a39f4662151a7821a5a628ba
joao <jovmamikl@gmail.com>
2026-02-13 10:44:03 -0300

    clean: arquivos movidos para pasta controller

--- src/main/java/Manager/TeatroManager/service/dto/ObraResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package Manager.TeatroManager.service.dto;
-
-import java.time.LocalDate;
-
-public record ObraResponseDTO(String nome, LocalDate dataAssistida, String local, 
-    String diretor, String elenco, String descricao,Integer nota) {
-    
-}
