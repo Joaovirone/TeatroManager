9c121e47192d089136af5b9c9478ffc9407685a2
joaovirone <jovmamikl@gmail.com>
2026-02-14 19:57:09 -0300

    feat: criado DTO para mudar senhas

---
+++ src/main/java/Manager/TeatroManager/controller/dto/UsuarioSenhaDTO.java
@@ -0,0 +1,14 @@
+package Manager.TeatroManager.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class UsuarioSenhaDTO {
+    
+    private String senhaAtual;
+    private String novaSenha;
+    private String confirmarSenha;
+
+
+
+}   
