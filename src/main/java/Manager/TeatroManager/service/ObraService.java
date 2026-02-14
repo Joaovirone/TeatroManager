437d21029045ab2a64c1daada1f0982aabbb958d
joaovirone <jovmamikl@gmail.com>
2026-02-13 22:35:03 -0300

    feat: implementado os métodos de criar e buscar obras

--- src/main/java/Manager/TeatroManager/service/ObraService.java
+++ src/main/java/Manager/TeatroManager/service/ObraService.java
@@ -1,12 +1,17 @@
 package Manager.TeatroManager.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import Manager.TeatroManager.controller.dto.ObraResponseDTO;
 import Manager.TeatroManager.entity.Obra;
 import Manager.TeatroManager.exception.EntityNotFoundException;
 import Manager.TeatroManager.repository.ObraRepository;
@@ -23,6 +28,13 @@ public class ObraService {
         return repository.save(obra);
     }
 
+
+    @Transactional(readOnly = true)
+    public List<Obra> buscarTodasAsObras(){
+        return repository.findAll();
+    }
+
+
     @Transactional(readOnly = true)
     public List<Obra> buscarObraPorNome (String nome){
         
@@ -33,9 +45,42 @@ public class ObraService {
             String.format("A obra de nome | %s | -- Não está cadastrada.", nome)
         );
       }
+      return obras;     
+    }
 
-      return obras;
+    @Transactional(readOnly = true)
+    public Obra buscarObraPorData(LocalDateTime data){
+      return repository.findObraByDate(data).
+            orElseThrow(()-> new RuntimeException("A data da obra | %s | -- Não está registrada no banco de dados"));
+
+ 
+    }
+
+    @Transactional(readOnly = true)
+    public List<Obra> buscarObraPorNomeDoDiretor (String nome){
         
+      List<Obra> diretor = repository.findObraByNomeDiretor(nome);
+
+      if(diretor.isEmpty()){
+        throw new EntityNotFoundException(
+            String.format("O diretor | %s | -- Não está registrado no banco de dados.", nome)
+        );
+      }
+      return diretor;  
+    }
+
+    @Transactional(readOnly = true)
+    public Obra buscarObraPorDescricao(String descricao){
+      return repository.findObraByDescricao(descricao).
+            orElseThrow(()-> new RuntimeException("A essa descrição | %s | -- Não está registrada no banco de dados"));
+
+ 
+    }
+
+
+    public Obra buscarTodasAsObras(ObraResponseDTO obraDTO) {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'buscarTodasAsObras'");
     }
 
 
