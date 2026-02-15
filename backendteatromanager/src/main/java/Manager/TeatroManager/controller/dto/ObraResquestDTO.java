6c79bf6b0bca1c7e1b55e573444c90e31b5a46c7
joaovirone <jovmamikl@gmail.com>
2026-02-15 17:29:17 -0300

    feat: implementado pattern de formatação de data

--- backendteatromanager/src/main/java/Manager/TeatroManager/controller/dto/ObraResquestDTO.java
+++ backendteatromanager/src/main/java/Manager/TeatroManager/controller/dto/ObraResquestDTO.java
@@ -1,7 +1,7 @@
 package Manager.TeatroManager.controller.dto;
 
 import java.time.LocalDate;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -13,6 +13,8 @@ public class ObraResquestDTO {
     @NotBlank
     @NotNull(message="Necessário nome da Obra !")
     String nome; 
+
+    @JsonFormat(pattern="yyy-MM-dd")
     LocalDate data; 
     String local; 
     String diretor; 
